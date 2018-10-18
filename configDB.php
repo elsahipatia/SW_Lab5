4c9f4bc50fb671db8b6b2369be64b3f8f4730c09
Daniel <cervera1994@gmail.com>
2018-10-18 18:02:02 +0200

    added configDB

---
+++ configDB.php
@@ -0,0 +1,15 @@
+<?php
+$local=1; //0 para la aplicación en 000WebHost
+if ($local==1){
+    $server="localhost";
+    $user="root";
+    $pass="";
+    $basededatos="quizz";
+}
+else{
+    $server="localhost";
+    $user="id7190553_root";
+    $pass="123456";
+    $basededatos="id7190553_quizz";
+}
+?>
